b3fefd94173c0805fb42d07a110a29f7083d6c5f
zyc <z35305515@gmail.com>
2016-01-21 01:53:25 +0800

    createQueue

--- index.js
+++ index.js
@@ -2,11 +2,12 @@
 * @Author: zyc
 * @Date:   2016-01-20 13:17:37
 * @Last Modified by:   zyc
-* @Last Modified time: 2016-01-20 22:24:59
+* @Last Modified time: 2016-01-21 01:51:14
 */
 'use strict'
 
 const fetchUrl = require('fetch').fetchUrl
+const convert = require('data2xml')()
 
 const Signature = require('./Signature')
 
@@ -28,13 +29,16 @@ module.exports = class {
     const method = 'PUT'
     const URI = `/queues/${queueName}`
     const { DATE, Authorization } = this.authorization({ VERB: method, CanonicalizedResource: URI })
+    options = options || {}
+    options._attr = { xmlns: 'http://mns.aliyuncs.com/doc/v1/' }
     fetchUrl(this.Endpoint + URI, {
       method,
       headers: {
         Date: DATE,
         Authorization,
         'x-mns-version': this.XMnsVersion
-      }
+      },
+      payload: convert('Queue', options)
     }, (err, res, buf) => {
       console.log(buf.toString())
     })
