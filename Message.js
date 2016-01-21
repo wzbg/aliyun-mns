ae879ccb0e6115fb735219fd4c6852dc27eb7db6
zyc <z35305515@gmail.com>
2016-01-21 22:45:27 +0800

    BatchSendMessage

--- Message.js
+++ Message.js
@@ -2,11 +2,10 @@
 * @Author: zyc
 * @Date:   2016-01-21 02:24:41
 * @Last Modified by:   zyc
-* @Last Modified time: 2016-01-21 19:18:11
+* @Last Modified time: 2016-01-21 22:44:04
 */
 'use strict'
 
-const assert = require('assert')
 const parser = require('xml2json')
 const convert = require('data2xml')()
 const fetchUrl = require('fetch').fetchUrl
@@ -19,18 +18,16 @@ module.exports = class {
     this.queue = queue
   }
 
-  send (options) {
-    assert.ok(options, 'options required')
-    assert.ok(options.MessageBody, 'MessageBody required')
+  send (message) {
     const method = 'POST'
     const URI = `/queues/${this.queue.name}/messages`
     const { DATE, Authorization } = this.mns.authorization({ VERB: method, CanonicalizedResource: URI })
-    options._attr = { xmlns }
+    message._attr = { xmlns }
     return new Promise((resolve, reject) => {
       fetchUrl(this.mns.Endpoint + URI, {
         method,
         headers: { Date: DATE, Authorization, 'x-mns-version': this.mns.XMnsVersion },
-        payload: convert('Message', options)
+        payload: convert('Message', message)
       }, (err, res, buf) => {
         if (err) return reject(err)
         const status = res.status
@@ -41,6 +38,31 @@ module.exports = class {
         }
         json.Message.status = status
         resolve(json.Message)
+      })
+    })
+  }
+
+  batchSend (messages) {
+    const method = 'POST'
+    const URI = `/queues/${this.queue.name}/messages`
+    const { DATE, Authorization } = this.mns.authorization({ VERB: method, CanonicalizedResource: URI })
+    messages = { Message: messages }
+    messages._attr = { xmlns }
+    return new Promise((resolve, reject) => {
+      fetchUrl(this.mns.Endpoint + URI, {
+        method,
+        headers: { Date: DATE, Authorization, 'x-mns-version': this.mns.XMnsVersion },
+        payload: convert('Messages', messages)
+      }, (err, res, buf) => {
+        if (err) return reject(err)
+        const status = res.status
+        const json = parser.toJson(buf.toString(), { object: true })
+        if (json.Error) {
+          json.Error.status = status
+          return reject(json.Error)
+        }
+        json.Messages.status = status
+        resolve(json.Messages)
       })
     })
   }
