054d90200e5a15f34fdd287a5b11f0ddf1f47876
zyc <z35305515@gmail.com>
2016-01-21 13:38:07 +0800

    Queue.create return

--- Queue.js
+++ Queue.js
@@ -2,14 +2,34 @@
 * @Author: zyc
 * @Date:   2016-01-20 23:16:03
 * @Last Modified by:   zyc
-* @Last Modified time: 2016-01-21 02:21:51
+* @Last Modified time: 2016-01-21 13:35:02
 */
 'use strict'
 
 const assert = require('assert')
+const parser = require('xml2json')
 const convert = require('data2xml')()
 const fetchUrl = require('fetch').fetchUrl
 
+const xmlns = 'http://mns.aliyuncs.com/doc/v1/'
+
+/*
+* DelaySeconds
+*  发送到该 Queue 的所有消息默认将以DelaySeconds参数指定的秒数延后可被消费，单位为秒。
+*  0-604800秒（7天）范围内某个整数值，默认值为0
+* MaximumMessageSize
+*  发送到该Queue的消息体的最大长度，单位为byte。
+*  1024(1KB)-65536（64KB）范围内的某个整数值，默认值为65536（64KB）。
+* MessageRetentionPeriod
+*  消息在该 Queue 中最长的存活时间，从发送到该队列开始经过此参数指定的时间后，不论消息是否被取出过都将被删除，单位为秒。
+*  60 (1分钟)-1296000 (15 天)范围内某个整数值，默认值345600 (4 天)
+* VisibilityTimeout
+*  消息从该 Queue 中取出后从Active状态变成Inactive状态后的持续时间，单位为秒。
+*  1-43200(12小时)范围内的某个值整数值，默认为30（秒）
+* PollingWaitSeconds
+*  当 Queue 中没有消息时，针对该 Queue 的 ReceiveMessage 请求最长的等待时间，单位为秒。
+*  0-30秒范围内的某个整数值，默认为0（秒）
+*/
 module.exports = class {
   constructor (mns, name, options) {
     assert.ok(name, 'name required')
@@ -23,17 +43,33 @@ module.exports = class {
     const URI = `/queues/${this.name}`
     const { DATE, Authorization } = this.mns.authorization({ VERB: method, CanonicalizedResource: URI })
     const options = this.options || {}
-    options._attr = { xmlns: 'http://mns.aliyuncs.com/doc/v1/' }
-    fetchUrl(this.mns.Endpoint + URI, {
-      method,
-      headers: {
-        Date: DATE,
-        Authorization,
-        'x-mns-version': this.mns.XMnsVersion
-      },
-      payload: convert('Queue', options)
-    }, (err, res, buf) => {
-      console.log(buf.toString())
+    options._attr = { xmlns }
+    return new Promise((resolve, reject) => {
+      fetchUrl(this.mns.Endpoint + URI, {
+        method,
+        headers: {
+          Date: DATE,
+          Authorization,
+          'x-mns-version': this.mns.XMnsVersion
+        },
+        payload: convert('Queue', options)
+      }, (err, res, buf) => {
+        if (err) return reject(err)
+        const status = res.status
+        const xml = buf.toString()
+        if (xml) {
+          const json = parser.toJson(xml, { object: true })
+          json.Error.status = status
+          return reject(json.Error)
+        }
+        return resolve({
+          xmlns,
+          Code: status === 201 ? 'Created' : 'No Content',
+          RequestId: res.responseHeaders['x-mns-request-id'],
+          HostId: this.mns.Endpoint,
+          status
+        })
+      })
     })
   }
 }
